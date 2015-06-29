5abf673c777629861b6bf44375230038ac3907d6
laxa <L4x4t0r@gmail.com>
2015-06-29 18:47:22 +0200

    Solver done!

--- solver.php
+++ solver.php
@@ -10,13 +10,88 @@ $grid = array(array(0, 4, 0, 1, 0, 0, 0, 0, 0),
               array(8, 1, 0, 0, 0, 2, 4, 0, 0),
               array(0, 0, 0, 0, 0, 4, 0, 9, 0));
 
-solveSudoku($grid);
+$solve = solveSudoku($grid);
+if (!$solve)
+    echo "Failed to solve grid\n";
+displayGrid($grid);
 
-function solveSukoku(&$grid, $x = 0, $y = 0)
+function solveSudoku(&$grid, $x = 0, $y = 0)
 {
     // if number already set in the grid
-    if ($grid[$x][$y] > 0)
+    if ($grid[$y][$x] > 0)
     {
-        
+        // if last number is already in the grid
+        if ($x === 8 && $y === 8) return true;
+        else if ($x === 8 && $y < 8) return solveSudoku($grid, 0, $y + 1);
+        return solveSudoku($grid, $x + 1, $y);
+    }
+    // otherwise, we need to find a number that match
+    else
+    {
+        $ret = false;
+        for ($i = 1; $i < 10 && !$ret; $i++)
+        {
+            if (canPlaceNumber($grid, $x, $y, $i))
+            {
+                $grid[$y][$x] = $i;
+                // stop the recursive calls when we completed the grid
+                if ($x === 8 && $y === 8) return true;
+                else if ($x === 8 && $y < 8) $ret = solveSudoku($grid, 0, $y + 1);
+                else $ret = solveSudoku($grid, $x + 1, $y);
+            }
+            // this will be executed in every call when the grid is complete
+            if ($ret) return true;
+        }
+        // we couldn't find a matching number, backtracking now
+        $grid[$y][$x] = 0;
+        return false;
+    }
+}
+
+function canPlaceNumber($grid, $x, $y, $i)
+{
+    // check columns
+    for ($index = 0; $index < 9; $index++)
+    {
+        if ($grid[$y][$index] === $i)
+            return false;
+    }
+    // check rows
+    for ($index = 0; $index < 9; $index++)
+    {
+        if ($grid[$index][$x] === $i)
+            return false;
+    }
+    // check square
+    if (in_array($y, [0, 1, 2]))
+        $indexY = 0;
+    else if (in_array($y, [3, 4, 5]))
+        $indexY = 3;
+    else
+        $indexY = 6;
+    if (in_array($x, [0, 1, 2]))
+        $indexX = 0;
+    else if (in_array($x, [3, 4, 5]))
+        $indexX = 3;
+    else
+        $indexX = 6;
+    $flagY = $indexY;
+    $flagX = $indexX;
+    for (; $indexY < $flagY + 3; $indexY++)
+    {
+        for (; $indexX < $flagX + 3; $indexX++)
+            if ($grid[$indexY][$indexX] === $i)
+                return false;
+    }
+    return true;
+}
+
+function displayGrid($grid)
+{
+    for ($y = 0; $y < 9; $y++)
+    {
+        for ($x = 0; $x < 9; $x++)
+            echo $grid[$y][$x];
+        echo "\n";
     }
 }
